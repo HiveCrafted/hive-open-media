648b72ae217ca75dcb3f2a242f81aec3a67c2c7b
Sandon Jurowski <jurowski@gmail.com>
2024-03-20 15:55:02 -0500

    typescript start and logo

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+const logo = require('./logo-1.png');
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Hive Open Media
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
